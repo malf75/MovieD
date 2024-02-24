4e12bc8e86a8c7db69675738e14739ca030d45cc
Marcos Lima <60903178+malf75@users.noreply.github.com>
2024-02-24 20:50:10 -0300

    teste38

--- config/settings.py
+++ config/settings.py
@@ -122,12 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'config/static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'config/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
