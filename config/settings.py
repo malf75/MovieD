703059a9c597aa556f7f7a2398175059a1146298
Marcos Lima <60903178+malf75@users.noreply.github.com>
2024-02-24 20:20:10 -0300

    teste35

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.movied.com.br','movied.com.br']
 
 
 # Application definition
