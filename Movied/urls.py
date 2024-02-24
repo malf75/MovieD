0fde0785955f775ab92c7ce9477b308bb1344cfc
Marcos Lima <60903178+malf75@users.noreply.github.com>
2024-02-24 03:52:27 -0300

    teste14

--- Movied/urls.py
+++ Movied/urls.py
@@ -3,8 +3,8 @@ from Movied import views
 
 urlpatterns = [
     path('api/filme_info/', views.get_filme_info, name='filme_info'),
-    path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
     path('profile/<int:pk>', views.profile, name='profile'),
