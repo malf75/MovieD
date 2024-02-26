439e27b903c3893758fb6c96f8360107652413a1
Marcos Lima <60903178+malf75@users.noreply.github.com>
2024-02-26 07:30:17 -0300

    teste65

--- Movied/views.py
+++ Movied/views.py
@@ -278,11 +278,10 @@ def profile_edit(request, pk):
                 profile.bio = bio
 
             if imagem:
-                name = f'{current_user.username}_{datetime.now()}.jpg'
-                file_path = f'media/imagens/{name}'
-                default_storage.save(file_path, imagem)
-
-                profile.profile_image = file_path
+                name = f'{current_user.username}_{datetime.now().jpg}'
+                profile.profile_image = imagem
+                file_path = f'images-movied/media/imagens/{imagem}'
+                default_storage.save(name, file_path)
 
             if erros:
                 context = {'erros': erros, 'profiles': profile}
