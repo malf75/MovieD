65ab50227d235a871015bcb9be618c8c85f5c4c0
Marcos Lima <60903178+malf75@users.noreply.github.com>
2024-03-09 22:12:27 -0300

    darkmode test

--- config/static/app/darkmode.js
+++ config/static/app/darkmode.js
@@ -1,46 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let butao = document.querySelector('#btn');
-  let logo = document.querySelector('.logo');
-  let logosign = document.querySelector('.logo__signup');
-
-  butao.addEventListener('click', () => {
-    if (butao.classList.contains('active')) {
-        disableDarkMode();
-    } else {
-        enableDarkMode();
-    }
-    });
-
-  const disableDarkMode = () => {
-      butao.classList.remove('active');
-      document.body.classList.remove('dark-mode');
-      if (logo) {
-          logo.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
-      }
-      if (logosign) {
-          logosign.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
-      }
-      localStorage.setItem('dark-mode', 'inactive');
-  }
-
-  const enableDarkMode = () => {
-      butao.classList.add('active');
-      document.body.classList.add('dark-mode');
-      if (logo) {
-          logo.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
-      }
-      if (logosign) {
-          logosign.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
-      }
-      localStorage.setItem('dark-mode', 'active');
-  }
+    let butao = document.querySelector('#btn');
+    let logo = document.querySelector('.logo');
+    let logosign = document.querySelector('.logo__signup');
   
-});
-
-let darkMode = localStorage.getItem('dark-mode');
-
-if (darkMode === 'active') {
-    enableDarkMode();
-} else {
-    disableDarkMode();
-}
+    const disableDarkMode = () => {
+        butao.classList.remove('active');
+        document.body.classList.remove('dark-mode');
+        if (logo) {
+            logo.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
+        }
+        if (logosign) {
+            logosign.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
+        }
+        localStorage.setItem('dark-mode', 'inactive');
+    }
+  
+    const enableDarkMode = () => {
+        butao.classList.add('active');
+        document.body.classList.add('dark-mode');
+        if (logo) {
+            logo.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
+        }
+        if (logosign) {
+            logosign.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
+        }
+        localStorage.setItem('dark-mode', 'active');
+    }
+  
+    let darkMode = localStorage.getItem('dark-mode');
+  
+    if (darkMode === 'active') {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
+    }
+  
+    butao.addEventListener('click', () => {
+      if (butao.classList.contains('active')) {
+          disableDarkMode();
+      } else {
+          enableDarkMode();
+      }
+    });
+  });
