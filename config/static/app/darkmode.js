c222565a1f7ffb62fd7f0fbe5cd554f49dd0cd1d
Marcos Lima <60903178+malf75@users.noreply.github.com>
2024-03-10 17:36:10 -0300

    test

--- config/static/app/darkmode.js
+++ config/static/app/darkmode.js
@@ -7,10 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
         butao.classList.remove('active');
         document.body.classList.remove('dark-mode');
         if (logo) {
-            logo.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
+            logo.src = 'https://movied-bucket.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
         }
         if (logosign) {
-            logosign.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
+            logosign.src = 'https://movied-bucket.s3.amazonaws.com/static/media/Design_sem_nome-removebg-preview.png';
         }
         localStorage.setItem('dark-mode', 'inactive');
     }
@@ -19,10 +19,10 @@ document.addEventListener('DOMContentLoaded', function() {
         butao.classList.add('active');
         document.body.classList.add('dark-mode');
         if (logo) {
-            logo.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
+            logo.src = 'https://movied-bucket.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
         }
         if (logosign) {
-            logosign.src = 'https://images-movied.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
+            logosign.src = 'https://movied-bucket.s3.amazonaws.com/static/media/Design_sem_nome__1_-removebg-preview.png';
         }
         localStorage.setItem('dark-mode', 'active');
     }
